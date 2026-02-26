f5830ac0106b36d91afaa7a8aa4ea69e4ef404c5
aryadneSC <aryadne.campos@ba.estudante.senai.br>
2026-02-26 10:19:03 -0300

    Last Sync: 2026-02-26 10:18 (Mobile)

--- JAVA/SENAI/Aula03_Exercicios_pt2/Macaco.java
+++ JAVA/SENAI/Aula03_Exercicios_pt2/Macaco.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Macaco {
     private String nome;
-    private list<String> estomago;
+    private List<String> estomago;
+    
+    public Macaco(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return this.nome;
+    }
 }
 
 public class Main {
 	public static void main(String[] args) {
 		
+		Macaco meuMacaco = new Macaco("Chico");
+		System.out.println("Macaco " + meuMacaco.getNome() + " nasceu!");
+		
+		//TODO add comer(String alimento), verEstomago()
 	}
 }
