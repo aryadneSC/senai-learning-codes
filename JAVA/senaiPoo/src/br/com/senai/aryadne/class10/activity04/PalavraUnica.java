801ed5e40026b986ff89638989c9213a7e3de44c
aryadneSC <aryadne.campos@ba.estudante.senai.br>
2026-05-14 14:12:20 -0300

    Adicionada atv 5 contagem de ocorrências com Map

--- JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity04/PalavraUnica.java
+++ JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity04/PalavraUnica.java
@@ -2,4 +2,9 @@ package br.com.senai.aryadne.class10.activity04;
 
 public class PalavraUnica {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity05/Ocorrencias.java
+++ JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity05/Ocorrencias.java
@@ -1,10 +1,39 @@
 package br.com.senai.aryadne.class10.activity05;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Ocorrencias {
+	// Indagação: como posso mapear a relação Strings e contagem das palavras
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite uma frase: ");
+		String frase = sc.nextLine().toLowerCase();
+		
+		String[] palavras = frase.split(" ");
+		
+		// Resposta: instanciar Map com generics p poder somar
+		// String, Integer p trabalhar com expressão matemática e evitar casting
+		Map <String, Integer> ocorrencias = new HashMap<>();
+		
+		for (String palavra : palavras) {
+			if (ocorrencias.containsKey(palavra)) {
+				// se existir, o valor atual é incrementado
+				ocorrencias.put(palavra, ocorrencias.get(palavra) + 1);
+			} else {
+				// se não, começa com 1
+				ocorrencias.put(palavra, 1);
+			}
+		}
+		
+		System.out.println("!-Ocorrências por palavra-!");
+		for (Map.Entry<String, Integer>entry : ocorrencias.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+		sc.close();
 
 	}
-
 }
