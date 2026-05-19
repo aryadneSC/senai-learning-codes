f9305e128a90dc7ea4cc4a25756d46c224b10cd9
aryadneSC <aryadne.campos@ba.estudante.senai.br>
2026-05-19 17:30:22 -0300

    feat: add atv 3 pilha:  expressao matemática parenteses

--- JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity03/Parenteses.java
+++ JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity03/Parenteses.java
@@ -1,5 +1,42 @@
+/*3- PILHA: Escreva um programa para verificar se uma 
+ * expressão matemática tem os parenteses agrupados de 
+ * forma correta.
+Ex.:
+((A + B) - INCORRETO
+A+B( - INCORRETO
+)A + B(-C - INCORRETO
+(A+B))-(C+D - INCORRETO
+((A + B)+D) - CORRETA
+*/
+
 package br.com.senai.aryadne.class10.activity03;
+
+import java.util.Map;
+import java.util.Stack;
 
 public class Parenteses {
 
+	public static boolean ehValida(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray() ) {
+			if (c == '(') {
+				stack.push(c);
+			} else if (c == ')') {
+				if (stack.isEmpty()) return false;
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
+	}
+
+	public static void main(String[] args) {
+			Parenteses p = new Parenteses();
+			String expressao = "((A + B)+D)";
+			
+			if (ehValida(expressao)) {
+				System.out.println("CORRETA");
+			} else {
+				System.out.println("INCORRETA");
+			}
+	}
 }
--- JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity04/PalavraUnica.java
+++ JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity04/PalavraUnica.java
@@ -1,3 +1,5 @@
+/*4- SET: Faça um programa que leia uma frase 
+ * e informe o número de palavras não repetidas da frase.*/
 package br.com.senai.aryadne.class10.activity04;
 
 public class PalavraUnica {
