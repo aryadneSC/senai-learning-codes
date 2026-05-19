fbe07a0ea17dc6d3b7eccadf2f97e3080c544a2c
aryadneSC <aryadne.campos@ba.estudante.senai.br>
2026-05-19 18:04:16 -0300

    feat: set: add atv 4 set

--- JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity04/PalavraUnica.java
+++ JAVA/senaiPoo/src/br/com/senai/aryadne/class10/activity04/PalavraUnica.java
@@ -2,11 +2,26 @@
  * e informe o número de palavras não repetidas da frase.*/
 package br.com.senai.aryadne.class10.activity04;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PalavraUnica {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static int contarPalavras(String s) {
+		String[] palavras = s.split(" ");
+		Set<String> naoRepetida = new HashSet<String>();
+		
+		for (String p : palavras) {
+			naoRepetida.add(p);
+		}
+		return naoRepetida.size();
 	}
 
+	public static void main(String[] args) {
+		String frase = "O carro para para estacionar";
+		PalavraUnica p = new PalavraUnica();
+		
+		System.out.println("'" + frase + "'" + " | Palavras não repetidas: ["
+				+ p.contarPalavras(frase) + "]");
+	}
 }
